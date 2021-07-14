4208dcf77b3c686d4fac672d3f42df1004bacecb
chenhe2018 <watch_ch@163.com>
2021-07-14 10:55:02 +0800

    '天之骄子找房'

--- config.py
+++ config.py
@@ -2,22 +2,18 @@
 
 
 mail = {
-    'sender': 'xxx@163.com',
+    'sender': 'watch_ch@163.com',
     'host': 'smtp.163.com',
-    'receivers': ['xxx@qq.com'],
-    'password': 'password',
-    'subject_prefix': '豆瓣爬虫租房'
+    'password': 'ch20103314',
+    'subject_prefix': '豆瓣爬虫租房',
+    'receivers': ['chenhe2018@qq.com'],
 }
 
 groups = [
-    (26926, '北京租房豆瓣'),
-    (279962, '北京租房（非中介）'),
-    (262626, '北京无中介租房（寻天使投资）'),
-    (35417, '北京租房'),
-    (56297, '北京个人租房 （真房源|无中介）'),
-    (257523, '北京租房房东联盟(中介勿扰) '),
+    (634832, '天之骄子'),
+    (496399, '【广兰路附近租房】'),
 ]
 
-locations = ('西二旗', '安宁庄', '小米', '上地', '龙泽', '永泰庄', '清河')
+locations = ('广兰路', '天之骄子')
 
-exclude_words = ('求租')
+exclude_words = ('求租', '天之骄子', '转租')
