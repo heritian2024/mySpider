6fe804e1d1b795676eaa746a48817d1842bf0744
chenhe2018 <watch_ch@163.com>
2021-08-11 09:55:41 +0800

    'fix'

---
+++ test4.py
@@ -0,0 +1,17 @@
+
+
+
+locations1 = ('广兰路', '天之骄子')
+locations2 = ['广兰路', '天之骄子']
+locations3 = ( '天之骄子')
+locations4 = [ '天之骄子']
+
+
+for l in locations1:
+    print(l)
+for l in locations2:
+    print(l)
+for l in locations3:
+    print(l)
+for l in locations4:
+    print(l)
